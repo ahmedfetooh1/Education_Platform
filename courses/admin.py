2664b3d9b53f1004270b01f0f8f4d6b4e7b827c2
AhmedAboElFetooh <ahmedfetooh54@gmail.com>
2025-08-24 10:26:22 +0300

    create models classes and added to admin dashboard

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Course , Subject , Module
 
 # Register your models here.
+
+@admin.register(Subject)
+class Subject(admin.ModelAdmin):
+    list_display =['title','slug']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class ModuleInline(admin.StackedInline):
+    model = Module
+
+
+@admin.register(Course)
+class Course(admin.ModelAdmin):
+    list_display =['title','subject','created','status']
+    list_filter = ['created','subject']
+    search_fields = ['title','overview']
+    prepopulated_fields = {'slug':('title',)}
+    inlines = [ModuleInline]
