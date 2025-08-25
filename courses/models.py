3a26ec14b49531b473fd3add855885c973709864
AhmedAboElFetooh <ahmedfetooh54@gmail.com>
2025-08-25 03:49:53 +0300

    create class that store (images , files , videos , text )

--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
 
 # Create your models here.
 
@@ -42,4 +45,36 @@ class Module(models.Model):
     def __str__(self) -> str :
         return str(self.title)
     
+
+class Content(models.Model):
+    module = models.ForeignKey(Module,related_name='content',on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType,on_delete=models.CASCADE , limit_choices_to={'model__in':('text','file','image','video')})
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type','object_id')
+
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User , related_name='%(class)s_related',on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta :
+        abstract = True
+    
+    def __str__(self) -> str :
+        return str(self.title)
+    
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+class Image(ItemBase):
+    image = models.FileField(upload_to='images')
+
+class Text(ItemBase):
+    content = models.TextField()
+
+class Video(ItemBase):
+    video = models.URLField()
+
 
